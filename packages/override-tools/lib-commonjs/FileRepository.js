50b8bcf705493b4df123865ff63e4deabe819451
Khalef1 <khalef.hosany@microsoft.com>
2020-06-09 05:28:29 -0700

    Merge master

---
+++ packages/override-tools/lib-commonjs/FileRepository.js
@@ -0,0 +1,20 @@
+"use strict";
+/**
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT License.
+ *
+ * @format
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.bindVersion = void 0;
+/**
+ * Convert from a VersionedReactFileRepository to ReactFileRepository
+ */
+function bindVersion(repository, version) {
+    return {
+        getFileContents: (filename) => repository.getFileContents(filename, version),
+        getVersion: () => version,
+    };
+}
+exports.bindVersion = bindVersion;
+//# sourceMappingURL=FileRepository.js.map
