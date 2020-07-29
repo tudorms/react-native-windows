1b83dd843afd5e630df96aed88a69f99cb69ec8e
tudorm <tudorm@microsoft.com>
2020-07-29 16:04:14 -0700

    Misc fixes (release build, STACK increase to 4Mb, assert fix)

--- vnext/QuickJSI/external/quickjspp.hpp
+++ vnext/QuickJSI/external/quickjspp.hpp
@@ -1221,7 +1221,7 @@ struct js_traits<Value>
 
     static JSValue wrap(JSContext * ctx, Value v) noexcept
     {
-        assert(JS_IsUndefined(v.v) || (ctx == v.ctx));
+        assert(JS_IsUndefined(v.v) || JS_IsNull(v.v) || (ctx == v.ctx));
         return v.release();
     }
 };
